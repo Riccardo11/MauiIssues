43f2ddc3b848cbe016ca73b0bafeb1adf07e2c64
riccardominato <minato.riccardo@gmail.com>
2023-01-27 14:27:01 +0100

    first commit

---
+++ ButtonWithGradientAndShadow/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ButtonWithGradientAndShadow;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ ButtonWithGradientAndShadow/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ButtonWithGradientAndShadow;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
